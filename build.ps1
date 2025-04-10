7dbc31b8193382155664412c38992a9696ea8298
jukebox1412 <jukebox1412@gmail.com>
2025-04-10 08:42:09 -0600

    moved some stuff around

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 $timestamp=Get-Date -Format "yyyyMMdd_HHmmss"
 $destination = "publish/publish-$timestamp.zip"
 
-Compress-Archive -Path "manifest.json", "youtube-music-extension.js", "README.md", "icons" -DestinationPath $destination
+Compress-Archive -Path "manifest.json", "youtube-music-extension.js", "README.md", "icon-48.png" -DestinationPath $destination
